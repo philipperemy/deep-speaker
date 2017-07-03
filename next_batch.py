e66704f998764422a05e4a6dffd05af0072c3902
philipperemy <premy@reactive.co.jp>
2017-07-03 15:49:25 +0900

    work in progress

--- next_batch.py
+++ next_batch.py
@@ -20,8 +20,45 @@ from librispeech_wav_reader import read_audio
 
 
 class MiniBatch:
-    def __init__(self, libri_batch):
-        self.libri_batch = libri_batch
+    def __init__(self, libri, batch_size):
+        # indices = np.random.choice(len(libri), size=batch_size, replace=False)
+        # [anc1, anc2, anc3, pos1, pos2, pos3, neg1, neg2, neg3]
+        # [sp1, sp2, sp3, sp1, sp2, sp3, sp4, sp5, sp6]
+
+        unique_speakers = list(libri['speaker_id'].unique())
+        num_triplets = batch_size // 3
+
+        anchor_batch = None
+        positive_batch = None
+        negative_batch = None
+        for ii in range(num_triplets):
+            two_different_speakers = np.random.choice(unique_speakers, size=2, replace=False)
+            anchor_positive_speaker = two_different_speakers[0]
+            negative_speaker = two_different_speakers[1]
+            anchor_positive_file = libri[libri['speaker_id'] == anchor_positive_speaker].sample(n=2, replace=False)
+            anchor_df = pd.DataFrame(anchor_positive_file[0:1])
+            anchor_df['training_type'] = 'anchor'
+            positive_df = pd.DataFrame(anchor_positive_file[1:2])
+            positive_df['training_type'] = 'positive'
+            negative_df = libri[libri['speaker_id'] == negative_speaker].sample(n=1)
+            negative_df['training_type'] = 'negative'
+
+            if anchor_batch is None:
+                anchor_batch = anchor_df.copy()
+            else:
+                anchor_batch = pd.concat([anchor_batch, anchor_df], axis=0)
+
+            if positive_batch is None:
+                positive_batch = positive_df.copy()
+            else:
+                positive_batch = pd.concat([positive_batch, positive_df], axis=0)
+
+            if negative_batch is None:
+                negative_batch = negative_df.copy()
+            else:
+                negative_batch = pd.concat([negative_batch, negative_df], axis=0)
+
+        self.libri_batch = pd.DataFrame(pd.concat([anchor_batch, positive_batch, negative_batch], axis=0))
         self.audio_loaded = False
 
     def load_wav(self):
@@ -32,6 +69,10 @@ class MiniBatch:
         self.audio_loaded = True
 
     def to_inputs(self):
+
+        if not self.audio_loaded:
+            self.load_wav()
+
         x = self.libri_batch['raw_audio'].values
         x = np.array([a.flatten() for a in x])
         y = self.libri_batch['speaker_id'].values
@@ -39,48 +80,10 @@ class MiniBatch:
 
 
 def stochastic_mini_batch(libri, batch_size):
-    # indices = np.random.choice(len(libri), size=batch_size, replace=False)
-    # [anc1, anc2, anc3, pos1, pos2, pos3, neg1, neg2, neg3]
-    # [sp1, sp2, sp3, sp1, sp2, sp3, sp4, sp5, sp6]
-
-    unique_speakers = list(libri['speaker_id'].unique())
-    num_triplets = batch_size // 3
-
-    anchor_batch = None
-    positive_batch = None
-    negative_batch = None
-    for ii in range(num_triplets):
-        two_different_speakers = np.random.choice(unique_speakers, size=2, replace=False)
-        anchor_positive_speaker = two_different_speakers[0]
-        negative_speaker = two_different_speakers[1]
-        anchor_positive_file = libri[libri['speaker_id'] == anchor_positive_speaker].sample(n=2, replace=False)
-        anchor_df = pd.DataFrame(anchor_positive_file[0:1])
-        anchor_df['training_type'] = 'anchor'
-        positive_df = pd.DataFrame(anchor_positive_file[1:2])
-        positive_df['training_type'] = 'positive'
-        negative_df = libri[libri['speaker_id'] == negative_speaker].sample(n=1)
-        negative_df['training_type'] = 'negative'
-
-        if anchor_batch is None:
-            anchor_batch = anchor_df.copy()
-        else:
-            anchor_batch = pd.concat([anchor_batch, anchor_df], axis=0)
-
-        if positive_batch is None:
-            positive_batch = positive_df.copy()
-        else:
-            positive_batch = pd.concat([positive_batch, positive_df], axis=0)
-
-        if negative_batch is None:
-            negative_batch = negative_df.copy()
-        else:
-            negative_batch = pd.concat([negative_batch, negative_df], axis=0)
-
-    libri_batch = pd.concat([anchor_batch, positive_batch, negative_batch], axis=0)
-    mini_batch = MiniBatch(libri_batch)
-    mini_batch.load_wav()
-    mini_batch.to_inputs()
-
+    mini_batch = MiniBatch(libri, batch_size)
+    return mini_batch
+    # mini_batch.load_wav()
+    # mini_batch.to_inputs()
 
 
 def test():
