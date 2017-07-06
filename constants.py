ebebb7f47da5091a46f2b3ac047398cbbed1f563
philipperemy <premy@reactive.co.jp>
2017-07-06 17:28:09 +0900

    work in progress

--- constants.py
+++ constants.py
@@ -2,8 +2,9 @@ DATASET_DIR = '/Volumes/Transcend/data-set/LibriSpeech'
 
 BATCH_NUM_TRIPLETS = 6  # should be a multiple of 3
 
+# very dumb values. I selected them to have a blazing fast training.
+# we will change them to their true values (to be defined?) later.
 NUM_FRAMES = 2
-
 SAMPLE_RATE = 100
 
 TRUNCATE_SOUND_FIRST_SECONDS = 1
