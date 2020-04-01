25557ab81277f3861dcb0fd3562a321d550cb3fa
Philippe Remy <premy.enseirb@gmail.com>
2020-04-01 11:33:21 +0900

    v4

--- download_vctk.sh
+++ download_vctk.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-wget http://homepages.inf.ed.ac.uk/jyamagis/release/VCTK-Corpus.tar.gz
-tar xvzf VCTK-Corpus.tar.gz -C ~
+endpoint="http://homepages.inf.ed.ac.uk/jyamagis/release/VCTK-Corpus.tar.gz"
+if [ -z "$(command -v axel)" ]
+then
+  wget ${endpoint}
+else
+  axel -n 10 ${endpoint} # faster download.
+fi
+ tar xvzf VCTK-Corpus.tar.gz -C ~
