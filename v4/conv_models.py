d13748a4657437a1d4b8c00027e1deaac7782af5
Philippe Remy <premy.enseirb@gmail.com>
2020-04-05 00:20:34 +0900

    refactoring

--- v4/conv_models.py
+++ v4/conv_models.py
@@ -10,102 +10,110 @@ from keras.layers.core import Reshape
 from keras.layers.normalization import BatchNormalization
 from keras.models import Model
 
-from constants import BATCH_SIZE, NUM_FRAMES
+from constants import BATCH_SIZE, NUM_FRAMES, NUM_FBANKS
 
-layers_dict = dict()
-
-
-def get(obj):
-    layer_name = obj.name
-    if layer_name not in layers_dict:
-        logging.info('-> Creating layer [{}]'.format(layer_name))
-        # create it
-        layers_dict[layer_name] = obj
-    else:
-        logging.info('-> Using layer [{}]'.format(layer_name))
-    return layers_dict[layer_name]
+logger = logging.getLogger(__name__)
 
 
-# no need to share the weights here because it does not exist.
-def clipped_relu(inputs):
-    return get(Lambda(lambda y: K.minimum(K.maximum(y, 0), 20), name='clipped_relu'))(inputs)
+class LayerCache:
+
+    def __init__(self):
+        self.layers_dict = dict()
+
+    def get(self, obj):
+        layer_name = obj.name
+        if layer_name not in self.layers_dict:
+            logger.info('-> Creating layer [{}]'.format(layer_name))
+            # create it
+            self.layers_dict[layer_name] = obj
+        else:
+            logger.info('-> Using layer [{}]'.format(layer_name))
+        return self.layers_dict[layer_name]
 
 
-def identity_block(input_tensor, kernel_size, filters, stage, block):
-    conv_name_base = 'res{}_{}_branch'.format(stage, block)
+class DeepSpeakerModel:
 
-    x = get(Conv2D(filters,
-                   kernel_size=kernel_size,
-                   strides=1,
-                   activation=None,
-                   padding='same',
-                   kernel_initializer='glorot_uniform',
-                   kernel_regularizer=regularizers.l2(l=0.0001),
-                   name=conv_name_base + '_2a'))(input_tensor)
-    x = get(BatchNormalization(name=conv_name_base + '_2a_bn'))(x)
-    x = clipped_relu(x)
+    def __init__(self, batch_input_shape=(BATCH_SIZE, NUM_FRAMES, NUM_FBANKS, 3)):
+        batch_size = batch_input_shape[0]
+        num_frames = batch_input_shape[1]
+        self.lc = LayerCache()
 
-    x = get(Conv2D(filters,
-                   kernel_size=kernel_size,
-                   strides=1,
-                   activation=None,
-                   padding='same',
-                   kernel_initializer='glorot_uniform',
-                   kernel_regularizer=regularizers.l2(l=0.0001),
-                   name=conv_name_base + '_2b'))(x)
-    x = get(BatchNormalization(name=conv_name_base + '_2b_bn'))(x)
+        # http://cs231n.github.io/convolutional-networks/
+        # conv weights
+        # #params = ks * ks * nb_filters * num_channels_input
 
-    x = layers.add([x, input_tensor])
-    x = clipped_relu(x)
-    return x
+        # Conv128-s
+        # 5*5*128*128/2+128
+        # ks*ks*nb_filters*channels/strides+bias(=nb_filters)
 
+        # take 100 ms -> 4 frames.
+        # if signal is 3 seconds, then take 100ms per 100ms and average out this network.
+        # 8*8 = 64 features.
 
-def convolutional_model(batch_input_shape=(BATCH_SIZE * 3 * NUM_FRAMES, 16, 16, 1),
-                        batch_size=BATCH_SIZE * 3, num_frames=NUM_FRAMES):
-    # http://cs231n.github.io/convolutional-networks/
-    # conv weights
-    # #params = ks * ks * nb_filters * num_channels_input
+        # used to share all the layers across the inputs
 
-    # Conv128-s
-    # 5*5*128*128/2+128
-    # ks*ks*nb_filters*channels/strides+bias(=nb_filters)
+        # num_frames = K.shape() - do it dynamically after.
+        inputs = Input(batch_shape=batch_input_shape)
+        x = self.cnn_component(inputs)
+        x = Reshape((2048,))(x)
+        x = Lambda(lambda y: K.reshape(y, (batch_size, num_frames, 2048)), name='reshape')(x)
+        x = Lambda(lambda y: K.mean(y, axis=1), name='average')(x)
+        x = Dense(512, name='affine')(x)  # .shape = (BATCH_SIZE * NUM_FRAMES, 512)
+        x = Lambda(lambda y: K.l2_normalize(y, axis=1), name='ln')(x)
+        self.m = Model(inputs, x, name='convolutional')
 
-    # take 100 ms -> 4 frames.
-    # if signal is 3 seconds, then take 100ms per 100ms and average out this network.
-    # 8*8 = 64 features.
+    def keras_model(self):
+        return self.m
 
-    # used to share all the layers across the inputs
+    # no need to share the weights here because it does not exist.
+    def clipped_relu(self, inputs):
+        return self.lc.get(Lambda(lambda y: K.minimum(K.maximum(y, 0), 20), name='clipped_relu'))(inputs)
 
-    # num_frames = K.shape() - do it dynamically after.
+    def identity_block(self, input_tensor, kernel_size, filters, stage, block):
+        conv_name_base = f'res{stage}_{block}_branch'
 
-    def conv_and_res_block(inp, filters, stage):
+        x = self.lc.get(Conv2D(filters,
+                               kernel_size=kernel_size,
+                               strides=1,
+                               activation=None,
+                               padding='same',
+                               kernel_initializer='glorot_uniform',
+                               kernel_regularizer=regularizers.l2(l=0.0001),
+                               name=conv_name_base + '_2a'))(input_tensor)
+        x = self.lc.get(BatchNormalization(name=conv_name_base + '_2a_bn'))(x)
+        x = self.clipped_relu(x)
+
+        x = self.lc.get(Conv2D(filters,
+                               kernel_size=kernel_size,
+                               strides=1,
+                               activation=None,
+                               padding='same',
+                               kernel_initializer='glorot_uniform',
+                               kernel_regularizer=regularizers.l2(l=0.0001),
+                               name=conv_name_base + '_2b'))(x)
+        x = self.lc.get(BatchNormalization(name=conv_name_base + '_2b_bn'))(x)
+
+        x = layers.add([x, input_tensor])
+        x = self.clipped_relu(x)
+        return x
+
+    def conv_and_res_block(self, inp, filters, stage):
         conv_name = 'conv{}-s'.format(filters)
-        o = get(Conv2D(filters,
-                       kernel_size=5,
-                       strides=2,
-                       padding='same',
-                       kernel_initializer='glorot_uniform',
-                       kernel_regularizer=regularizers.l2(l=0.0001), name=conv_name))(inp)
-        o = get(BatchNormalization(name=conv_name + '_bn'))(o)
-        o = clipped_relu(o)
+        o = self.lc.get(Conv2D(filters,
+                               kernel_size=5,
+                               strides=2,
+                               padding='same',
+                               kernel_initializer='glorot_uniform',
+                               kernel_regularizer=regularizers.l2(l=0.0001), name=conv_name))(inp)
+        o = self.lc.get(BatchNormalization(name=conv_name + '_bn'))(o)
+        o = self.clipped_relu(o)
         for i in range(3):
-            o = identity_block(o, kernel_size=3, filters=filters, stage=stage, block=i)
+            o = self.identity_block(o, kernel_size=3, filters=filters, stage=stage, block=i)
         return o
 
-    def cnn_component(inp):
-        x_ = conv_and_res_block(inp, 64, stage=1)
-        x_ = conv_and_res_block(x_, 128, stage=2)
-        x_ = conv_and_res_block(x_, 256, stage=3)
-        x_ = conv_and_res_block(x_, 512, stage=4)
-        return x_
-
-    # TODO the network should be definable without explicit batch shape
-    inputs = Input(batch_shape=batch_input_shape)
-    x = cnn_component(inputs)
-    x = Reshape((2048,))(x)
-    x = Lambda(lambda y: K.reshape(y, (batch_size, num_frames, 2048)), name='reshape')(x)
-    x = Lambda(lambda y: K.mean(y, axis=1), name='average')(x)
-    x = Dense(512, name='affine')(x)  # .shape = (BATCH_SIZE * NUM_FRAMES, 512)
-    x = Lambda(lambda y: K.l2_normalize(y, axis=1), name='ln')(x)
-    m = Model(inputs, x, name='convolutional')
-    return m
+    def cnn_component(self, inp):
+        x = self.conv_and_res_block(inp, 64, stage=1)
+        x = self.conv_and_res_block(x, 128, stage=2)
+        x = self.conv_and_res_block(x, 256, stage=3)
+        x = self.conv_and_res_block(x, 512, stage=4)
+        return x
