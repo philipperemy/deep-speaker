ec1640149d2249ecd4ffa3cbd64becc33148d734
Philippe Remy <premy.enseirb@gmail.com>
2020-04-04 23:56:00 +0900

    refactoring

--- v4/cli.py
+++ v4/cli.py
@@ -8,13 +8,13 @@ import click
 from audio import Audio
 from batcher import KerasConverter, FBankProcessor
 from constants import SAMPLE_RATE, NUM_FRAMES
-from train_cli import start_training
+from train import start_training
 from utils import ClickType as Ct
 from utils import init_pandas, create_new_empty_dir
 
 logger = logging.getLogger(__name__)
 
-VERSION = '1.0a'
+VERSION = '1.0b'
 
 
 @click.group()
