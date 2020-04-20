b7bcc04f0e328a65e2611d946f607b1b0a9953b6
Philippe Remy <premy.enseirb@gmail.com>
2020-04-20 18:22:06 +0900

    much cleaner setup

--- run-remote.sh
+++ run-remote.sh
@@ -14,10 +14,10 @@ cd "${WORKING_DIR}" && bash download_librispeech.sh && cd -
 #python cli.py libri-to-vctk-format --libri "${LIBRI_SPEECH_DATASET}" --output "${TRIPLET_TRAINING_WORKING_DIR}/audio"
 
 # Pre-training (0.92k speakers).
-python cli.py build-mfcc-cache --working_dir "${PRE_TRAINING_WORKING_DIR}"
+python cli.py build-mfcc-cache --working_dir "${PRE_TRAINING_WORKING_DIR}" --audio_dir "${WORKING_DIR}/LibriSpeech/train-clean-360"
 python cli.py build-keras-inputs --working_dir "${PRE_TRAINING_WORKING_DIR}"
 python cli.py train-model --working_dir "${PRE_TRAINING_WORKING_DIR}" --pre_training_phase
 
 # Triplet-training (2.48k speakers).
-python cli.py build-mfcc-cache --working_dir "${TRIPLET_TRAINING_WORKING_DIR}"
+python cli.py build-mfcc-cache --working_dir "${TRIPLET_TRAINING_WORKING_DIR}" --audio_dir "${WORKING_DIR}/LibriSpeech"
 python cli.py train-model --working_dir "${TRIPLET_TRAINING_WORKING_DIR}"
