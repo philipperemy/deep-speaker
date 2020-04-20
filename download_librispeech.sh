343eed25df73c53e683bd659e2119f88f6ab75d4
Philippe Remy <premy.enseirb@gmail.com>
2020-04-20 19:41:43 +0900

    chmod

--- download_librispeech.sh
+++ download_librispeech.sh
@@ -3,18 +3,26 @@
 set -e
 
 # axel or wget.
-axel -n 10 -a http://www.openslr.org/resources/12/dev-clean.tar.gz
-axel -n 10 -a http://www.openslr.org/resources/12/dev-other.tar.gz
-axel -n 10 -a http://www.openslr.org/resources/12/test-clean.tar.gz
-axel -n 10 -a http://www.openslr.org/resources/12/test-other.tar.gz
-axel -n 10 -a http://www.openslr.org/resources/12/train-clean-100.tar.gz
-axel -n 10 -a http://www.openslr.org/resources/12/train-clean-360.tar.gz
-axel -n 10 -a http://www.openslr.org/resources/12/train-other-500.tar.gz
+# axel -n 10 -a http://www.openslr.org/resources/12/dev-clean.tar.gz
+# axel -n 10 -a http://www.openslr.org/resources/12/dev-other.tar.gz
+# axel -n 10 -a http://www.openslr.org/resources/12/test-clean.tar.gz
+# axel -n 10 -a http://www.openslr.org/resources/12/test-other.tar.gz
+# axel -n 10 -a http://www.openslr.org/resources/12/train-clean-100.tar.gz
+# axel -n 10 -a http://www.openslr.org/resources/12/train-clean-360.tar.gz
+# axel -n 10 -a http://www.openslr.org/resources/12/train-other-500.tar.gz
 
-tar xvzf dev-clean.tar.gz
-tar xvzf dev-other.tar.gz
-tar xvzf test-clean.tar.gz
-tar xvzf test-other.tar.gz
-tar xvzf train-clean-100.tar.gz
-tar xvzf train-clean-360.tar.gz
-tar xvzf train-other-500.tar.gz
+echo "extracting dev-clean..."
+tar xzf dev-clean.tar.gz
+echo "extracting dev-other..."
+tar xzf dev-other.tar.gz
+echo "extracting test-clean..."
+tar xzf test-clean.tar.gz
+echo "extracting test-other..."
+tar xzf test-other.tar.gz
+echo "extracting train-clean-100..."
+tar xzf train-clean-100.tar.gz
+echo "extracting train-clean-360..."
+tar xzf train-clean-360.tar.gz
+echo "extracting train-other-500..."
+tar xzf train-other-500.tar.gz
+
--- run-remote.sh
+++ run-remote.sh
@@ -8,19 +8,19 @@ TRIPLET_TRAINING_WORKING_DIR="${WORKING_DIR}/triplet-training"
 
 mkdir -p "${WORKING_DIR}"
 
-# WORKING_DIR/LibriSpeech
+# Download and extract the LibriSpeech dataset.
 cp download_librispeech.sh "${WORKING_DIR}"
 cd "${WORKING_DIR}" && bash download_librispeech.sh && cd -
 
-# LIBRI_SPEECH_DATASET="${WORKING_DIR}/LibriSpeech"
-#python cli.py libri-to-vctk-format --libri "${LIBRI_SPEECH_DATASET}" --output "${PRE_TRAINING_WORKING_DIR}/audio" --subset train-clean-360
-#python cli.py libri-to-vctk-format --libri "${LIBRI_SPEECH_DATASET}" --output "${TRIPLET_TRAINING_WORKING_DIR}/audio"
+# Build MFCC caches for pre-training and triplet-training.
+python cli.py build-mfcc-cache --working_dir "${PRE_TRAINING_WORKING_DIR}" --audio_dir "${WORKING_DIR}/LibriSpeech/train-clean-360"
+python cli.py build-mfcc-cache --working_dir "${TRIPLET_TRAINING_WORKING_DIR}" --audio_dir "${WORKING_DIR}/LibriSpeech"
+
+# Build the Keras inputs for the pre-training.
+# python cli.py build-keras-inputs --working_dir "${PRE_TRAINING_WORKING_DIR}"
 
 # Pre-training (0.92k speakers).
-python cli.py build-mfcc-cache --working_dir "${PRE_TRAINING_WORKING_DIR}" --audio_dir "${WORKING_DIR}/LibriSpeech/train-clean-360"
-python cli.py build-keras-inputs --working_dir "${PRE_TRAINING_WORKING_DIR}"
-python cli.py train-model --working_dir "${PRE_TRAINING_WORKING_DIR}" --pre_training_phase
+# python cli.py train-model --working_dir "${PRE_TRAINING_WORKING_DIR}" --pre_training_phase
 
 # Triplet-training (2.48k speakers).
-python cli.py build-mfcc-cache --working_dir "${TRIPLET_TRAINING_WORKING_DIR}" --audio_dir "${WORKING_DIR}/LibriSpeech"
-python cli.py train-model --working_dir "${TRIPLET_TRAINING_WORKING_DIR}"
+# python cli.py train-model --working_dir "${TRIPLET_TRAINING_WORKING_DIR}"
