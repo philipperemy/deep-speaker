b60a27071d13dd42e0187ab231717b5f6bdaa9ad
Philippe Remy <premy.enseirb@gmail.com>
2020-04-24 15:26:29 +0900

    uncomment

--- download_librispeech.sh
+++ download_librispeech.sh
@@ -3,13 +3,14 @@
 set -e
 
 # axel or wget.
-# axel -n 10 -a http://www.openslr.org/resources/12/dev-clean.tar.gz
-# axel -n 10 -a http://www.openslr.org/resources/12/dev-other.tar.gz
-# axel -n 10 -a http://www.openslr.org/resources/12/test-clean.tar.gz
-# axel -n 10 -a http://www.openslr.org/resources/12/test-other.tar.gz
-# axel -n 10 -a http://www.openslr.org/resources/12/train-clean-100.tar.gz
-# axel -n 10 -a http://www.openslr.org/resources/12/train-clean-360.tar.gz
-# axel -n 10 -a http://www.openslr.org/resources/12/train-other-500.tar.gz
+#  axel -n 10 -a [file] or wget [file]
+wget http://www.openslr.org/resources/12/dev-clean.tar.gz
+wget http://www.openslr.org/resources/12/dev-other.tar.gz
+wget http://www.openslr.org/resources/12/test-clean.tar.gz
+wget http://www.openslr.org/resources/12/test-other.tar.gz
+wget http://www.openslr.org/resources/12/train-clean-100.tar.gz
+wget http://www.openslr.org/resources/12/train-clean-360.tar.gz
+wget http://www.openslr.org/resources/12/train-other-500.tar.gz
 
 echo "extracting dev-clean..."
 tar xzf dev-clean.tar.gz
