bc5ba6d15b7864de62bf41db1c05c566c4314cdc
Philippe Remy <premy.enseirb@gmail.com>
2020-04-24 15:19:22 +0900

    example

--- example.py
+++ example.py
@@ -1,10 +1,13 @@
 import numpy as np
-
+import random
 from audio import read_mfcc
 from batcher import sample_from_mfcc
 from constants import SAMPLE_RATE, NUM_FRAMES
 from conv_models import DeepSpeakerModel
 from test import batch_cosine_similarity
+
+np.random.seed(123)
+random.seed(123)
 
 model = DeepSpeakerModel()
 model.m.load_weights('/Users/premy/deep-speaker/checkpoints/ResCNN_triplet_training_checkpoint_175.h5', by_name=True)
