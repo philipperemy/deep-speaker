ad6deae705f6a05067ce2cbdddb061d358823b72
Philippe Rémy <premy.enseirb@gmail.com>
2020-05-23 14:46:46 +0900

    Update cli.py

--- cli.py
+++ cli.py
@@ -45,6 +45,9 @@ def build_audio_cache(working_dir, audio_dir, sample_rate):
 @click.option('--working_dir', required=True, type=Ct.input_dir())
 @click.option('--counts_per_speaker', default='600,100', show_default=True, type=str)  # train,test
 def build_keras_inputs(working_dir, counts_per_speaker):
+    # counts_per_speaker: If you specify --counts_per_speaker 600,100, that means for each speaker, 
+    # you're going to generate 600 samples for training and 100 for testing. One sample is 160 frames 
+    # by default (~roughly 1.6 seconds).
     counts_per_speaker = [int(b) for b in counts_per_speaker.split(',')]
     kc = KerasFormatConverter(working_dir)
     kc.generate(max_length=NUM_FRAMES, counts_per_speaker=counts_per_speaker)
