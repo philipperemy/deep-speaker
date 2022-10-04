3421d2b31f7c76b38f74da4ee743123ee2a7c9d0
philipperemy <premy.enseirb@gmail.com>
2022-10-04 11:09:47 +0700

    add features from the Paulo Raca features

--- example.py
+++ example.py
@@ -1,12 +1,12 @@
 import random
-from test import batch_cosine_similarity
 
 import numpy as np
 
 from audio import read_mfcc
 from batcher import sample_from_mfcc
-from constants import NUM_FRAMES, SAMPLE_RATE
+from constants import SAMPLE_RATE, NUM_FRAMES
 from conv_models import DeepSpeakerModel
+from test import batch_cosine_similarity
 
 # Reproducible results.
 np.random.seed(123)
@@ -16,33 +16,27 @@ random.seed(123)
 model = DeepSpeakerModel()
 
 # Load the checkpoint.
-model.m.load_weights("ResCNN_triplet_training_checkpoint_265.h5", by_name=True)
+model.m.load_weights('ResCNN_triplet_training_checkpoint_265.h5', by_name=True)
 
 # Sample some inputs for WAV/FLAC files for the same speaker.
 # To have reproducible results every time you call this function, set the seed every time before calling it.
 # np.random.seed(123)
 # random.seed(123)
-mfcc_001 = sample_from_mfcc(
-    read_mfcc("samples/PhilippeRemy/PhilippeRemy_001.wav", SAMPLE_RATE), NUM_FRAMES
-)
-mfcc_002 = sample_from_mfcc(
-    read_mfcc("samples/PhilippeRemy/PhilippeRemy_002.wav", SAMPLE_RATE), NUM_FRAMES
-)
+mfcc_001 = sample_from_mfcc(read_mfcc('samples/PhilippeRemy/PhilippeRemy_001.wav', SAMPLE_RATE), NUM_FRAMES)
+mfcc_002 = sample_from_mfcc(read_mfcc('samples/PhilippeRemy/PhilippeRemy_002.wav', SAMPLE_RATE), NUM_FRAMES)
 
 # Call the model to get the embeddings of shape (1, 512) for each file.
 predict_001 = model.m.predict(np.expand_dims(mfcc_001, axis=0))
 predict_002 = model.m.predict(np.expand_dims(mfcc_002, axis=0))
 
 # Do it again with a different speaker.
-mfcc_003 = sample_from_mfcc(
-    read_mfcc("samples/1255-90413-0001.flac", SAMPLE_RATE), NUM_FRAMES
-)
+mfcc_003 = sample_from_mfcc(read_mfcc('samples/1255-90413-0001.flac', SAMPLE_RATE), NUM_FRAMES)
 predict_003 = model.m.predict(np.expand_dims(mfcc_003, axis=0))
 
 # Compute the cosine similarity and check that it is higher for the same speaker.
 same_speaker_similarity = batch_cosine_similarity(predict_001, predict_002)
 diff_speaker_similarity = batch_cosine_similarity(predict_001, predict_003)
-print("SAME SPEAKER", same_speaker_similarity)  # SAME SPEAKER [0.81564593]
-print("DIFF SPEAKER", diff_speaker_similarity)  # DIFF SPEAKER [0.1419204]
+print('SAME SPEAKER', same_speaker_similarity)  # SAME SPEAKER [0.81564593]
+print('DIFF SPEAKER', diff_speaker_similarity)  # DIFF SPEAKER [0.1419204]
 
 assert same_speaker_similarity > diff_speaker_similarity
